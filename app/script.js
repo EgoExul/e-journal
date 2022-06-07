ad73912f5c964368dda5caf6b15dcddc7c58079e
EgoExul <goo.mihan@gmail.com>
2022-06-07 19:19:14 +0400

    Made corrections, added a drop-down list of groups, without transition. profile icon. Rating.

--- app/script.js
+++ app/script.js
@@ -51,3 +51,26 @@ function enable() {
             document.getElementById("score11").disabled = false; document.getElementById("score-11").disabled = false;
 
         };
+
+
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+        for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
